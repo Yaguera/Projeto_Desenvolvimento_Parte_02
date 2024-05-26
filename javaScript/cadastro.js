e77ad2b6ea337491ab2e03186131442f3e852522
KauanAnjos <kauandosanjosmeneses@gmail.com>
2024-05-25 23:18:26 -0300

    Ajuste na tela de cadastro

--- javaScript/cadastro.js
+++ javaScript/cadastro.js
@@ -1,9 +1,10 @@
 const passwordInput = document.querySelector("#passwordInput");
 
-passwordInput.addEventListener("input", () => {
+passwordInput.addEventListener("input", function () {
     
-    const password = this.CSSMathValue;
-    const strengthIndicator = document.querySelector("#password-strength-indicador");
+    const password = this.value;
+    console.log(password)
+   const strengthIndicator = document.querySelector("#password-strength-indicator");
 
     const strengthText = document.querySelector("#password-strength-text");
 
@@ -14,7 +15,27 @@ passwordInput.addEventListener("input", () => {
         3: "Forte",
         4: "Muito Forte"
     };
+    console.log(strengths)
 
     let score = 0;
+
+    // Requisitos
+    if (password.length >= 8) score++;
+    if (password.match(/[a-z]/)) score++;
+    if (password.match(/[A-Z]/)) score++;
+    if (password.match(/[0-9]/)) score++;
+    if (password.match(/[^a-z]/)) score++;
+
+
+    // Calculo da %
+    const width = (score / 4) * 100;
+
+    strengthIndicator.style.width =  `${width}%`;
+
+    if(password.length > 0) {
+        strengthText.innerHTML = `Força: ${strengths[score]}`;
+    } else {
+        strengthText.innerHTML = ""
+    }
 
 });
